64c5ac641326ae84086884072b52c738aba8470e
Gabriel Moraga <agarom97@gmail.com>
2021-01-20 16:16:01 +0100

    Aggiunti console log

--- es5/giorno2/js/script.js
+++ es5/giorno2/js/script.js
@@ -10,10 +10,14 @@ function functionName(array, min, max) {
 
 var arrayA = [0,1,2,3,4,5,6];
 
+console.log('Primo array ', arrayA);
+
 var minIndex = 1;
 
 var maxIndex = 5;
 
+console.log('L indice minimo è ' + minIndex + ' e l indice massimo è ' + maxIndex);
+
 var arrayB = functionName(arrayA, minIndex, maxIndex);
 
-console.log(arrayB);
+console.log('Nuovo array creato', arrayB);
--- es6/giorno2/js/script.js
+++ es6/giorno2/js/script.js
@@ -6,10 +6,14 @@ const myFunction = (array, min, max) => {
 
 const arrayA = [0,1,2,3,4,5,6]
 
+console.log('Primo array', arrayA);
+
 const minIndex = 1;
 
 const maxIndex = 5;
 
+console.log(`L'indice minimo è ${minIndex} e l'indice massimo è ${maxIndex}`);
+
 const arrayB = myFunction(arrayA, minIndex, maxIndex)
 
-console.log(arrayB);
+console.log('Nuovo array creato', arrayB);
